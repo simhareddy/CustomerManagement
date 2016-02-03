93723bb8e9d3756e3f4e43b1d1a3a83fdb319b1b
Narasimha Reddy <n.a.reddy.peddireddy@accenture.com>
2016-02-03 17:18:01 +0530

    Add and Modify customer changes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,10 +8,10 @@
  *
  * Main module of the application.
  */
-var app = angular
+var customerManagementApp = angular
   .module('customerManagementApp', ['ngRoute', 'angularUtils.directives.dirPagination']);
   
-  app.config(function ($routeProvider) {
+  customerManagementApp.config(function ($routeProvider) {
     $routeProvider
       $routeProvider.
             when('/', {
@@ -31,8 +31,8 @@ var app = angular
                 redirectTo: '/'
             });
   });
-  
-app.controller('getDataController', function($rootScope,$scope, $http){
+// Controller for get the data from json file  
+customerManagementApp.controller('getDataController', function($rootScope,$scope, $http){
 
    $http.get('/customers/')
         .success(function (data) {
@@ -41,8 +41,9 @@ app.controller('getDataController', function($rootScope,$scope, $http){
         });
 }); 
  
-app.controller('customerAppController', function($rootScope,$scope){
-	
+// Controller for Add, Modify and Delete functionalities
+customerManagementApp.controller('customerAppController', function($rootScope,$scope){
+	// Creating a pagination object with default values
 	$scope.pagination = {
 		perPage: 12,
 		perPageOptions: [12, 24, 50],
@@ -60,6 +61,7 @@ app.controller('customerAppController', function($rootScope,$scope){
 	
 	$scope.visibleCustomers = this.visibleCustomers ;
 		
+	// When page changed visible customers should be update	
 	$scope.pageChanged = function(newPage){
   
 		$scope.pagination.currentPage = newPage;
@@ -71,10 +73,10 @@ app.controller('customerAppController', function($rootScope,$scope){
 		$scope.visibleCustomers = this.visibleCustomers ;
 	};
 		
-	
-	$scope.delete = function(index){
+	// Delete customer
+	$scope.delete = function(customer){
         
-        $scope.customers.splice( $scope.customers.indexOf(index),1);
+        $scope.customers.splice( $scope.customers.indexOf(customer),1);
 	
 		if($scope.pagination.currentPage != $scope.pagination.total)
 			this.visibleCustomers = ($scope.pagination.currentPage * $scope.pagination.perPage);
@@ -84,63 +86,49 @@ app.controller('customerAppController', function($rootScope,$scope){
 		$scope.visibleCustomers = this.visibleCustomers ;
     };
 	
-	
-	
+	// Add customer 	
 	$scope.addCustomer = function() {
 	
 	var id = ($rootScope.customers[$rootScope.customers.length-1].id + 1);
-    var customerDetails = {'id': id,'Name': $scope.customer.name, 'Location': $scope.customer.location, 'orders': $scope.customer.orders, 'image':'/images/User.jpg'};
-    
+    var customerDetails = {'id': id,'Name': $scope.customer.Name, 'Location': $scope.customer.Location, 'orders': $scope.customer.orders, 'image':$scope.customer.image}
 	$rootScope.customers.push(customerDetails);
     
-	console.log($rootScope.customers);
-    $scope.customer.name= "";
-    $scope.customer.location = "";
+	// console.log($rootScope.customers);
+    $scope.customer.Name= "";
+    $scope.customer.Location = "";
     $scope.customer.orders= "";
+	$scope.customer.image = "";
 	
 	document.getElementById("addCustomer").innerHTML = 'Hi, Customer added successfully. ';
-    /* var messageElement = angular.element( document.querySelector( '#addCustomer' ) );
-    messageElement.append('Hi, Customer added successfully. '); */
+    
   };
   
   $scope.model = {
 	  selected: {}
   };
-  
+  // Template change functionality for edit and display 
   $scope.getTemplate = function (customer) {
-	 // console.log('Inside getTemplate: '+customer.Name);
-	  //console.log('Inside getTemplate selected: '+$scope.model.selected.Name);
-	  //console.log($scope.model.selected.id);
-	  //console.log(customer.id);
-		if($scope.model.selected.id == undefined){
+	 if($scope.model.selected.id == undefined){
 		  return 'display';
 		}		
-        else if (customer.id === $scope.model.selected.id) return 'edit';
-        else return 'display';
+     else if (customer.id === $scope.model.selected.id) return 'edit';
+     else return 'display';
     };
-	
+
+// For edit customer 	
 	$scope.editCustomer = function (customer) {
-		console.log('inside edit method');
-		//console.log(customer);
-        $scope.model.selected = angular.copy(customer);
+	    $scope.model.selected = angular.copy(customer);
 		$scope.getTemplate(customer);
     };
-
+// Update the customer 
     $scope.saveCustomer = function (customer) {
-        console.log("Saving customer"+customer.id);
+		// $scope.model.selected = angular.copy(customer);      
 		var data = $scope.customers;
-		console.log(data);
 		var idx = data.map(function(d) { return d['id']; }).indexOf(customer.id);
-		//var idx = $scope.customers.indexOf();
-		console.log('index::'+idx);
-		var customerDetails = {'id': customer.id,'Name': $scope.model.selected.customer.Name, 'Location': $scope.model.selected.customer.Location, 'orders': $scope.model.selected.customer.orders, 'image':'/images/User.jpg'};
-		$scope.model.selected = customerDetails;
-        $scope.customers[idx] = angular.copy($scope.model.selected);
-		console.log($scope.customers[idx]);
-		console.log(angular.copy($scope.model.selected));
-        $scope.reset();
+		$scope.customers[idx] = angular.copy(customer);
+		$scope.reset();
     };
-
+// Reset the model object
     $scope.reset = function () {
         $scope.model.selected = {};
     };
